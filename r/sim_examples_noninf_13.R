8975308839cf3c17df06e65ea37f62d554acaf11
James Totterdell <jatotterdell@gmail.com>
2019-07-21 16:57:52 +0800

    Change dir.

--- r/sim_examples_noninf_13.R
+++ r/sim_examples_noninf_13.R
@@ -6,5 +6,5 @@ for(i in 1:13) {
     sims = 2, nA = 3, nB = 4, p = p0, n_max = 1e4, burn_in = 500, n_update = 500,
     eps = 0.15, non_thres = 0.5, sup_thres = 0.85, drop_thres_best = 0.01, brar = 3,
     fix_ctrl = 1/13, Sigma0 = diag(10, 13))
-  saveRDS(res, paste0("sim_out/noninf_13/", sprintf("%02d", i - 1), "_effect_scenario1.rds"))
+  saveRDS(res, paste0("sim_out/", sprintf("%02d", i - 1), "_effect_scenario1.rds"))
 }
